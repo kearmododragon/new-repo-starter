64fa7271cf43a18169b38a62ee6adb7e6f3ff509
kearmododragon <ckearney1992@gmail.com>
2023-02-22 15:54:33 +0100

    answered

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+Maastricht, Netherlands
 // 2. Have you lived anywhere else? If so where? 
-
+Nottingha & Stoke in England, Barcelona Spain, Rio De Janiero Brazil, Maastricht Netherlands
 // 3. What was your most recent job? What did you enjoy about that role?
-
+Logistics specialist at Mercedes-Benz. I like that they pay me on time now
 // 4. What is your "comfort" food? 
-
+Pizza
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+I travel a lot and like to put others first.
